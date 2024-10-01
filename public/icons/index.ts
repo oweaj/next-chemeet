e56df4a44805d7220c30c010d389ba935a09e0b7
oweaj <qwe1520156@naver.com>
2024-10-01 15:57:38 +0900

    [Chore] svg 명칭 중복 변경

--- public/icons/index.ts
+++ public/icons/index.ts
@@ -6,7 +6,7 @@ export { default as AlarmIcon } from "./AlarmIcon.svg";
 export { default as Kakao } from "./login/Kakao.svg";
 export { default as Google } from "./login/Google.svg";
 export { default as Github } from "./login/Github.svg";
-export { default as SocialLogin } from "./login/SocialLogin.svg";
+export { default as SocialLoginLine } from "./login/SocialLoginLine.svg";
 export { default as PasswordCheck } from "./login/PasswordCheck.svg";
 export { default as PasswordHide } from "./login/PasswordHide.svg";
 export { default as PasswordShow } from "./login/PasswordShow.svg";
