b03fedb29e677e92dff9576f337ea6dc5bbe2b1e
oweaj <qwe1520156@naver.com>
2024-10-01 16:00:51 +0900

    [Chore] 소셜 로그인 수정 및 로고 링크 추가

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,12 +1,23 @@
 import Link from "next/link";
 import Image from "next/image";
-import { SocialLogin, Logo } from "@public/icons";
-import { LoginForm, SocialLoginForm } from "../_components/LoginForm";
+import { SocialLoginLine, Logo } from "@public/icons";
+import { LoginForm } from "../_components/LoginForm";
+import SocialLogin from "../_components/SocialLogin";
+import { GithubLogin, GoogleLogin, KakaoLogin } from "@/lib/actions/authAction";
+import { Google, Kakao, Github } from "@public/icons";
+
+const socialLoginList = [
+  { action: KakaoLogin, provider: "kakao", icon: Kakao },
+  { action: GoogleLogin, provider: "google", icon: Google },
+  { action: GithubLogin, provider: "github", icon: Github },
+];
 
 export default function Login() {
   return (
     <>
-      <Image src={Logo} alt="logo" />
+      <Link href={"/"}>
+        <Image src={Logo} alt="logo" />
+      </Link>
       <LoginForm />
       <div className="flex items-center gap-3 text-sm">
         <Link href="/find">이메일 찾기</Link>
@@ -14,8 +25,14 @@ export default function Login() {
         <Link href="/pw-reset">비밀번호 찾기</Link>
       </div>
       <>
-        <Image src={SocialLogin} alt="간편 로그인 이미지" />
-        <SocialLoginForm />
+        <Image src={SocialLoginLine} alt="간편 로그인 이미지" />
+        <div className="flex items-center justify-center gap-4">
+          {socialLoginList.map((item) => (
+            <form action={item.action} key={item.provider}>
+              <SocialLogin {...item} />
+            </form>
+          ))}
+        </div>
       </>
     </>
   );
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,11 +2,14 @@ import Image from "next/image";
 import { Logo } from "@public/icons";
 import RegisterForm from "../_components/RegisterForm";
 import AuthWrap from "../_components/AuthWrap";
+import Link from "next/link";
 
 export default function Register() {
   return (
     <AuthWrap style="gap-3 p-6 xxl:gap-5 duration-300">
-      <Image src={Logo} alt="logo" />
+      <Link href={"/"}>
+        <Image src={Logo} alt="logo" />
+      </Link>
       <h1 className="text-xl font-semibold">이메일로 회원가입</h1>
       <RegisterForm />
     </AuthWrap>
